eb35a682ac9a86cffdf559c959ee82b83baf9c7b
Lamark <lamark12ricarte@gmail.com>
2024-02-07 10:40:16 -0300

    mock, http add: POST, PUT, DELETE

--- rest-with-spring-boot/src/main/java/br/com/lamarkes/restwithspringboot/controllers/PersonController.java
+++ rest-with-spring-boot/src/main/java/br/com/lamarkes/restwithspringboot/controllers/PersonController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -13,13 +14,40 @@ public class PersonController {
 
     @Autowired
     private PersonService service;
-
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return service.findAll();
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findBdyId(
-            @PathVariable(value = "id") String id)throws Exception{
+            @PathVariable(value = "id") String id) {
 
         return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(
+            @RequestBody Person person) {
+
+        return service.createPerson(person);
+    }
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(
+            @RequestBody Person person) {
+
+        return service.updatePerson(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(
+            @PathVariable(value = "id") String id) {
+
+        service.deletePerson(id);
     }
 
 }
--- rest-with-spring-boot/src/main/java/br/com/lamarkes/restwithspringboot/services/PersonService.java
+++ rest-with-spring-boot/src/main/java/br/com/lamarkes/restwithspringboot/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.lamarkes.restwithspringboot.services;
 import br.com.lamarkes.restwithspringboot.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -14,6 +16,15 @@ public class PersonService {
 
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
+    public List<Person> findAll(){
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id){
         logger.info("Finding one Person!");
         Person person = new Person();
@@ -21,6 +32,39 @@ public class PersonService {
         person.setFirstName("Lamark");
         person.setLastName("Ricarte");
         person.setAddress("Uberlandia - Minas Gerais - Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    public Person createPerson(Person person){
+
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public Person updatePerson(Person person){
+
+        logger.info("Creating one Person!");
+
+        return person;
+    }
+
+    public void deletePerson(String id){
+
+        logger.info("Deleting one Person!");
+
+
+    }
+
+    private Person mockPerson(int i) {
+        logger.info("Finding one Person!");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name: " +i);
+        person.setLastName("Last name" +i);
+        person.setAddress("Some address in Brasil: "+i);
         person.setGender("Male");
 
         return person;
